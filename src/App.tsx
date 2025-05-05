f47ac5b9a9ff4b705a2b17480571ff7032c5ee63
Cristiano Souza <ltianosouzal@gmail,con>
2025-05-05 14:28:55 -0300

    add lucide react and modify app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { TimerIcon } from "lucide-react";
+import { Heading } from "./components/Heading";
+
 export default function App() {
   return (
-    <div>
-      <h1>My React App</h1>
-      <p>This is a simple React application.</p>
-    </div>
+    <>
+      <Heading>
+        Hello World
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
+    </>
   );
 }
